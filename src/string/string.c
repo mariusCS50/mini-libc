8d29130e46bdc50c0ff15c5e3621743c04ba244f
marius.gaibu <marius.gaibu@stud.acs.upb.ro>
2024-10-21 18:22:54 +0300

    Implemented string-handling functions

--- src/string/string.c
+++ src/string/string.c
@@ -4,38 +4,45 @@
 
 char *strcpy(char *destination, const char *source)
 {
-	/* TODO: Implement strcpy(). */
-	return destination;
+  while (*destination++ = *source++);
+
+  return destination;
 }
+
 
 char *strncpy(char *destination, const char *source, size_t len)
 {
-	/* TODO: Implement strncpy(). */
+  while (len-- && (*destination++ = *source++));
+
 	return destination;
 }
 
 char *strcat(char *destination, const char *source)
 {
-	/* TODO: Implement strcat(). */
+	strcpy(destination + strlen(destination), source);
+
 	return destination;
 }
 
 char *strncat(char *destination, const char *source, size_t len)
 {
-	/* TODO: Implement strncat(). */
+  destination += strlen(destination);
+	strncpy(destination, source, len);
+  *(destination + len) = '\0';
+
 	return destination;
 }
 
 int strcmp(const char *str1, const char *str2)
 {
-	/* TODO: Implement strcmp(). */
-	return -1;
+  for (; *str1 && *str1 == *str2; str1++, str2++);
+  return *str1 - *str2;
 }
 
 int strncmp(const char *str1, const char *str2, size_t len)
 {
-	/* TODO: Implement strncmp(). */
-	return -1;
+	for (; --len && *str1 && *str1 == *str2; str1++, str2++);
+  return *str1 - *str2;
 }
 
 size_t strlen(const char *str)
@@ -50,48 +57,61 @@ size_t strlen(const char *str)
 
 char *strchr(const char *str, int c)
 {
-	/* TODO: Implement strchr(). */
-	return NULL;
+	for (; *str && *str != c; str++);
+	return (*str == c ? str : NULL);
 }
 
 char *strrchr(const char *str, int c)
 {
-	/* TODO: Implement strrchr(). */
-	return NULL;
+  char *last_occ = NULL;
+	for (; *str; str++) {
+    last_occ = (*str == c ? (char *)str : last_occ);
+  }
+	return (*str == c ? str : last_occ);
 }
 
 char *strstr(const char *haystack, const char *needle)
 {
-	/* TODO: Implement strstr(). */
+  int steps = strlen(haystack) - strlen(needle);
+  while (steps--) {
+    if (strncmp(haystack, needle, strlen(needle)) == 0) return (char *)haystack;
+    haystack++;
+  }
 	return NULL;
 }
 
 char *strrstr(const char *haystack, const char *needle)
 {
-	/* TODO: Implement strrstr(). */
-	return NULL;
+	int steps = strlen(haystack) - strlen(needle);
+  char *last_occ = NULL;
+  while (steps--) {
+    if (strncmp(haystack, needle, strlen(needle)) == 0) last_occ = (char *)haystack;;
+    haystack++;
+  }
+	return last_occ;
 }
 
 void *memcpy(void *destination, const void *source, size_t num)
 {
-	/* TODO: Implement memcpy(). */
+  strncpy((char *)destination, (char *)source, num);
 	return destination;
 }
 
 void *memmove(void *destination, const void *source, size_t num)
 {
-	/* TODO: Implement memmove(). */
+	char buffer[256];
+  strcpy(buffer, source);
+  strncpy((char *)destination, buffer, num);
 	return destination;
 }
 
 int memcmp(const void *ptr1, const void *ptr2, size_t num)
 {
-	/* TODO: Implement memcmp(). */
-	return -1;
+	return strncmp((char *)ptr1, (char *)ptr2, num);
 }
 
 void *memset(void *source, int value, size_t num)
 {
-	/* TODO: Implement memset(). */
+	for (; num--; *(char *)source++ = value);
 	return source;
 }
